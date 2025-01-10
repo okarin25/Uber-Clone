780fb7e90f87f72404c105216f668f9e958a9992
Arpit Vishwakarma <110905960+okarin25@users.noreply.github.com>
2025-01-10 14:27:27 +0530

    third commit

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser');
 
 // Establish MongoDB connection
 connectToDb();
@@ -13,6 +14,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+pp.use(cookieParser());
 
 // Define a test route
 app.get('/', (req, res) => {
