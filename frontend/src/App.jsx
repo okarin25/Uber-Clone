1bccbbceeef7204c972d909cf112dbb0fae74432
Arpit Vishwakarma <110905960+okarin25@users.noreply.github.com>
2025-01-14 14:22:39 +0530

    seventh commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup from './pages/CaptainSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import Home from './pages/Home'
+import UserLogout from './pages/UserLogout'
 
 const App = () => {
   return (
@@ -9,8 +16,13 @@ const App = () => {
         <Route path = '/login' element ={<UserLogin/>} />
         <Route path = '/signup' element ={<UserSignup/>} />
         <Route path = '/captain-login' element ={<CaptainLogin/>} />
-          <Route path = '/captain-signup' element ={<CaptainSignup/>} />
-
+        <Route path = '/captain-signup' element ={<CaptainSignup/>} />
+        <Route path = '/home' element ={
+          <UserProtectWrapper>
+            <Home/>
+          </UserProtectWrapper>
+        } />
+        <Route path = '/user/logout' element ={UserLogout} />
       </Routes>
     </div>
   )
