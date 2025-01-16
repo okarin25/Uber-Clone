1bfc3257aaa8a2922f17aca2f3655b3eed4780d7
Arpit Vishwakarma <110905960+okarin25@users.noreply.github.com>
2025-01-16 13:35:06 +0530

    eighth commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import UserContext from './context/UserContext.jsx';
-import CaptainContext from './context/CapatainContext.jsx';
-
+import CaptainContext from './context/CaptainContext.jsx';
+import SocketProvider from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
 
   <CaptainContext>
     <UserContext>
+      <SocketProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </SocketProvider>
     </UserContext>
   </CaptainContext>
 
